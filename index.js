d6bb52d568d9536bec7ee4b1a8818d8889d40727
Matthew Shepard <mattshepard@gmail.com>
2015-09-30 16:28:59 -0700

    Initial modularization refactor

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 'use strict';
 
+var _ = require('lodash');
 var board = require('./lib/board');
 
 function bs2serial(app, opts, cb){
 
   var namespace = opts.namespace || 'bs2serial';
 
-  app.expose(namespace, board);
-  app.addBoard('bs2', board);
+  var Board = _.clone(board);
+  Board.Programmer = opts.programmer.Programmer;
+  Board.Identify = opts.programmer.identify;
+  Board.Revisions = opts.programmer.revisions;
+  Board.Protocol = opts.protocol;
+  Board.Transport = opts.transport;
+  Board.Tokenizer = opts.tokenizer;
+
+  app.expose(namespace, Board);
+  app.addBoard('bs2', Board);
 
   cb();
 }
